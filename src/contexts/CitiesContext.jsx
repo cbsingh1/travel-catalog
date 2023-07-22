cf3d69bd8772f4448c9087d489d262e80e6d26c5
Chandra Bhan Singh <cbhansingh1@gmail.com>
2023-07-22 23:21:15 +0800

    Performance optimization via useCallback

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,4 +1,10 @@
-import { createContext, useEffect, useContext, useReducer } from "react";
+import {
+  createContext,
+  useEffect,
+  useContext,
+  useReducer,
+  useCallback,
+} from "react";
 
 const BASE_URL = "http://localhost:9000";
 
@@ -63,6 +69,7 @@ function CitiesProvider({ children }) {
   useEffect(function () {
     async function fetchCities() {
       dispatch({ type: "loading" });
+
       try {
         const res = await fetch(`${BASE_URL}/cities`);
         const data = await res.json();
@@ -77,7 +84,8 @@ function CitiesProvider({ children }) {
     fetchCities();
   }, []);
 
-  async function getCity(id) {
+  const getCity = useCallback(
+    async function getCity(id) {
       if (Number(id) === currentCity.id) return;
 
       dispatch({ type: "loading" });
@@ -90,9 +98,11 @@ function CitiesProvider({ children }) {
         dispatch({
           type: "rejected",
           payload: "There was an error loading the city...",
-      });
-    }
-  }
+        });
+      }
+    },
+    [currentCity.id]
+  );
 
   async function createCity(newCity) {
     dispatch({ type: "loading" });
